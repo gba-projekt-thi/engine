701f6346a488d88b3923fccd2a06f6cea68ceab1
MaximilianBe <maximilianberning04@gmail.com>
2026-05-11 14:59:44 +0200

    removed general_collision_detection for merge

--- src/general_collision_detection/graphics_files/tilemap_8_8/simple_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="simple_tileset" tilewidth="8" tileheight="8" tilecount="100" columns="10">
- <image source="../simple_tileset.bmp" width="80" height="80"/>
-</tileset>
