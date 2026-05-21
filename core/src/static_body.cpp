db854834d1a096b47355854be20abb117613c09b
neow <neow@neows-MacBook.local>
2026-05-21 12:26:27 +0200

    get rid of make warnings

--- core/src/static_body.cpp
+++ core/src/static_body.cpp
@@ -1,8 +1,8 @@
 #include "static_body.h"
 
 StaticBody::StaticBody(bn::fixed x, bn::fixed y, bn::fixed w, bn::fixed h,
-                       uint16_t layers)
-    : pos{x, y, w, h}, layers(layers) {
+                       uint16_t layers_)
+    : pos{x, y, w, h}, layers(layers_) {
     CollisionRegistry::instance().register_body(this);
 }
 
